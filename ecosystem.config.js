dd45d99479a4a05a1bff1c011b4d6a227d376832
Yacin Bahi <yacin@red64.io>
2023-03-14 16:47:27 +0100

    trying to run pnpm on remote server

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,16 +4,17 @@ module.exports = {
     name: 'indexing-service',
   }],
    
-  // Note: we're running a production script on the staging server!
+  // Note: the indexer script runs on the staging server but 
+  //       writes to the production database!
   deploy : {
     production : {
        "user" : "ubuntu",
        "host" : "ec2-18-221-245-11.us-east-2.compute.amazonaws.com",
        "key"  : "~/.ssh/superpower.pem",
-      //  "ref"  : "origin/main",
-       "ref"  : "origin/run-indexer-with-node-cron",
+       "ref"  : "origin/main",
        "repo" : "git@github.com:superpowerlabs/event-scraper",
        "path" : "/home/ubuntu/production/indexing-service",
+       "post-setup": "/home/ubuntu/.nvm/versions/node/v16.17.0/bin/pnpm i",
        "post-deploy" : "pnpm install && pm2 startOrRestart --env production"
     }
   }
