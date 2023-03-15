d60f5ab657df3cd15d7c19e071eb4ab3a1bb69b9
roquesrolando <roquesrolando@hotmail.com>
2023-03-15 14:56:53 -0400

    added rename column name and insert id increment

--- src/db/migrations/2_MigrateDataFromT1toT2.js
+++ src/db/migrations/2_MigrateDataFromT1toT2.js
@@ -8,13 +8,29 @@ class MigrateDataFromT1toT2 extends require("../Migration") {
     // await sql.schema.dropTableIfExists("test");
 
     if (await sql.schema.hasTable("syn_city_passes_transfer")) {
-      // alter table to add increments("id").primary()
-      // rename column
-      await sql.schema.raw(`
-      
-      `);
-      done = true;
-      debug('Table "test" created.');
+      try {
+        await sql.schema.raw("alter table syn_city_passes_transfer add column id serial primary key;");
+        await sql.schema.raw(`alter table syn_city_passes_transfer rename column tokenid to token_id;`);
+        done = true;
+        debug('Table "syn_city_passes_transfer" edited.');
+      } catch (e) {
+        done = false;
+      }
+    }
+
+    if (!done) {
+      debug("No change required for this migration");
+    }
+
+    if (await sql.schema.hasTable("syn_city_coupons_transfer")) {
+      try {
+        await sql.schema.raw("alter table syn_city_coupons_transfer add column id serial primary key;");
+        await sql.schema.raw(`alter table syn_city_coupons_transfer rename column tokenid to token_id;`);
+        done = true;
+        debug('Table "syn_city_coupons_transfer" edited.');
+      } catch (e) {
+        done = false;
+      }
     }
 
     if (!done) {
