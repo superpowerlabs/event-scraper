c267b565cc955d41800add0c47761677b4831ce7
roquesrolando <roquesrolando@hotmail.com>
2023-03-16 14:57:23 -0400

    added check for column

--- src/db/migrations/2_MigrateDataFromT1toT2.js
+++ src/db/migrations/2_MigrateDataFromT1toT2.js
@@ -8,28 +8,31 @@ class MigrateDataFromT1toT2 extends require("../Migration") {
     // await sql.schema.dropTableIfExists("test");
 
     if (await sql.schema.hasTable("syn_city_passes_transfer")) {
-      try {
-        await sql.schema.raw("alter table syn_city_passes_transfer add column id serial primary key;");
+      if (await sql.schema.hasColumn("syn_city_passes_transfer", "tokenid")) {
         await sql.schema.raw(`alter table syn_city_passes_transfer rename column tokenid to token_id;`);
         done = true;
-        debug('Table "syn_city_passes_transfer" edited.');
-      } catch (e) {
-        done = false;
+        debug('Table "syn_city_passes_transfer" tokenid changed to token_id.');
+      }
+      if (!(await sql.schema.hasColumn("syn_city_passes_transfer", "id"))) {
+        await sql.schema.raw("alter table syn_city_passes_transfer add column id serial primary key;");
+        done = true;
+        debug('Table "syn_city_passes_transfer" column id created.');
       }
     }
-
     if (!done) {
       debug("No change required for this migration");
     }
 
     if (await sql.schema.hasTable("syn_city_coupons_transfer")) {
-      try {
-        await sql.schema.raw("alter table syn_city_coupons_transfer add column id serial primary key;");
+      if (await sql.schema.hasColumn("syn_city_coupons_transfer", "tokenid")) {
         await sql.schema.raw(`alter table syn_city_coupons_transfer rename column tokenid to token_id;`);
         done = true;
-        debug('Table "syn_city_coupons_transfer" edited.');
-      } catch (e) {
-        done = false;
+        debug('Table "syn_city_coupons_transfer" tokenid changed to token_id.');
+      }
+      if (!(await sql.schema.hasColumn("syn_city_coupons_transfer", "id"))) {
+        await sql.schema.raw("alter table syn_city_coupons_transfer add column id serial primary key;");
+        done = true;
+        debug('Table "syn_city_coupons_transfer" column id created.');
       }
     }
 
