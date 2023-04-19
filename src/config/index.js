b6d74bc25b97180a90431a6275bc4100af384b37
Francesco Sullo <sullof@gmail.com>
2023-04-19 16:18:03 -0700

    Adding alternative config to use it in other projects with different configurations

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,33 @@
 const ethers = require("ethers");
+const path = require("path");
+const fs = require("fs-extra");
+
+let contracts = require("./deployedProduction.json");
+let eventsByContract = require("./eventsByContract");
+
+let providers = {
+  1: new ethers.providers.InfuraProvider("homestead", process.env.INFURA_KEY),
+  56: new ethers.providers.JsonRpcProvider("https://bscrpc.com", 56),
+};
+
+let averageBlockPerDay = {
+  1: 240,
+  56: 1200,
+  137: 1800,
+};
+
+// checks if there are overrides
+const configOverridePath = path.join(__dirname, "../config-override.js");
+if (fs.existsSync(configOverridePath)) {
+  const configOverride = require(configOverridePath);
+  contracts = configOverride.contracts;
+  eventsByContract = configOverride.eventsByContract;
+  providers = { ...providers, ...(configOverride.providers || {}) };
+  averageBlockPerDay = {
+    ...averageBlockPerDay,
+    ...(configOverride.averageBlockPerDay || {}),
+  };
+}
 
 const config = {
   typeMapping: {
@@ -6,17 +35,10 @@ const config = {
     uint256: "NUMERIC(78, 0)",
     bool: "BOOLEAN",
   },
-  contracts: require("./deployedProduction.json"),
-  eventsByContract: require("./eventsByContract"),
-  providers: {
-    1: new ethers.providers.InfuraProvider("homestead", process.env.INFURA_KEY),
-    56: new ethers.providers.JsonRpcProvider("https://bscrpc.com", 56),
-  },
-  averageBlockPerDay: {
-    1: 240,
-    56: 1200,
-    137: 1800,
-  },
+  contracts,
+  eventsByContract,
+  providers,
+  averageBlockPerDay,
 };
 
 module.exports = config;
