d226f207fc3a210b8aa0906062895fb285863c51
Francesco Sullo <sullof@gmail.com>
2024-03-13 19:30:12 -0700

    Add new contract

--- src/config/eventsByContract.js
+++ src/config/eventsByContract.js
@@ -615,7 +615,8 @@ const eventsByContract = {
       },
       {
         name: "MintAndStakeRequested",
-        filter: "MintAndStakeRequested(uint256,uint256,address,uint256,uint256)",
+        filter:
+          "MintAndStakeRequested(uint256,uint256,address,uint256,uint256)",
         ABI: ABI.MintAndStakeRequested,
       },
       {
@@ -637,6 +638,17 @@ const eventsByContract = {
     ],
   },
   PFPAssetPolygon: {
+    chainId: 137,
+    startBlock: 0,
+    events: [
+      {
+        name: "Transfer",
+        filter: "Transfer(address,address,uint256)",
+        ABI: ABI.Transfer,
+      },
+    ],
+  },
+  PFPAssetP2: {
     chainId: 137,
     startBlock: 0,
     events: [
