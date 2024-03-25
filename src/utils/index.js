2a608ece346f0726f74db02aae737d01a48e4aa9
roquesrolando <roquesrolando@hotmail.com>
2024-03-25 13:02:21 -0400

    added migration using db

--- src/utils/index.js
+++ src/utils/index.js
@@ -7,17 +7,18 @@ const utils = {
     return new Promise((resolve) => setTimeout(resolve, millis));
   },
 
-  nameTable(contractName, filter) {
+  nameTable(contractName, filter, version) {
     let attrs = filter
       .split("(")[1]
       .split(")")[0]
       .split(",")
       .map((e) => e.substring(0, 1));
-    return [
-      Case.snake(contractName),
-      Case.snake(filter.split("(")[0]),
-      attrs.join(""),
-    ].join("__");
+
+    let parts = [Case.snake(contractName), Case.snake(filter.split("(")[0]), attrs.join("")];
+    if (version !== "" && version) {
+      parts.splice(1, 0, version);
+    }
+    return parts.join("__");
   },
 
   nameTableV1(contractName, eventName) {
